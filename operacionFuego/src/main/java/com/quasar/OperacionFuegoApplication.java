010c498dc02a9df9b7247205143904266bea85ee
juanpablo527 <juanpablo527@hotmail.com>
2022-05-13 10:55:17 -0500

    fisrst Commit

---
+++ operacionFuego/src/main/java/com/quasar/OperacionFuegoApplication.java
@@ -0,0 +1,13 @@
+package com.quasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperacionFuegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperacionFuegoApplication.class, args);
+	}
+
+}
---
+++ operacionFuego/src/test/java/com/quasar/OperacionFuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperacionFuegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
